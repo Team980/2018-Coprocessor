114427890977d05eeb0b485ffceb3fc52a4b8c3f
Luke Myers <19lmyers@gmail.com>
2018-02-06 21:05:12 -0800

    Add power cube detection to Pixy

--- Rioduino.ino
+++ Rioduino.ino
@@ -12,20 +12,26 @@ unsigned long previousMillis = 0;
 
 Pixy pixy;
 
-int visionTargetCoord = -1;
-int rangedDistance = 0;
+int visionTargetCoord;
+int powerCubeWidth;
+int powerCubeHeight;
+int powerCubeCoord;
+int rangedDistance;
 
 void setup() {
-  visionTargetCoord = -1;
+  visionTargetCoord = -2;
+  powerCubeWidth = 0;
+  powerCubeHeight = 0;
+  powerCubeCoord = 0;
   rangedDistance = 0;
-  
+
   Wire.begin(4);
   Wire.onRequest(requestEvent);
-  
+
   pixy.init();
-  
+
   sonarSerial.begin(9600);
-  
+
   Serial.begin(115200);
   Serial.println("Starting...");
 }
@@ -34,11 +40,30 @@ void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= 10) {
     previousMillis = currentMillis;
-    
-    if (pixy.getBlocks(2)) {
-      visionTargetCoord = (pixy.blocks[0].x + pixy.blocks[1].x) / 2;
+
+    int numBlocks = pixy.getBlocks();
+    //Serial.println((String) "numBlocks: " + numBlocks);
+    if (numBlocks > 0) {
+      visionTargetCoord = -2;
+      powerCubeWidth = 0;
+      powerCubeHeight = 0;
+      powerCubeCoord = 0;
+      for (int i = 0; i < numBlocks; i++) {
+        //Serial.println((String) i + ": " + pixy.blocks[i].signature);
+        if (pixy.blocks[i].signature == 1) { //Vision target
+          visionTargetCoord += pixy.blocks[i].x;
+        } else if (pixy.blocks[i].signature == 2)  { //Power cube
+          if (pixy.blocks[i].width > powerCubeWidth) { //We want the biggest one
+            //Serial.println((String) "Width: " + pixy.blocks[i].width);
+            powerCubeWidth = pixy.blocks[i].width;
+            powerCubeHeight = pixy.blocks[i].height;
+            powerCubeCoord = pixy.blocks[i].x;
+          }
+        }
+      }
+      visionTargetCoord = visionTargetCoord / 2; //There should always be 2 of them
     }
-    
+
     boolean msgComplete = false;
     int index = 0;
     char buff[5];
@@ -59,26 +84,42 @@ void loop() {
         index = 0;
         msgComplete = true;
         rangedDistance = atoi(buff);
+        //Serial.println((String) "rangedDistance: " + rangedDistance);
       }
     }
-    
+
   }
 }
 
 void requestEvent() {
-  byte buff[8];
-  
+  byte buff[20];
+
   buff[0] = visionTargetCoord >> 24;
   buff[1] = visionTargetCoord >> 16;
   buff[2] = visionTargetCoord >> 8;
   buff[3] = visionTargetCoord;
-  
-  buff[4] = rangedDistance >> 24;
-  buff[5] = rangedDistance >> 16;
-  buff[6] = rangedDistance >> 8;
-  buff[7] = rangedDistance;
-  
-  Wire.write(buff, 8);
+
+  buff[4] = powerCubeWidth >> 24;
+  buff[5] = powerCubeWidth >> 16;
+  buff[6] = powerCubeWidth >> 8;
+  buff[7] = powerCubeWidth;
+
+  buff[8] = powerCubeHeight >> 24;
+  buff[9] = powerCubeHeight >> 16;
+  buff[10] = powerCubeHeight >> 8;
+  buff[11] = powerCubeHeight;
+
+  buff[12] = powerCubeCoord >> 24;
+  buff[13] = powerCubeCoord >> 16;
+  buff[14] = powerCubeCoord >> 8;
+  buff[15] = powerCubeCoord;
+
+  buff[16] = rangedDistance >> 24;
+  buff[17] = rangedDistance >> 16;
+  buff[18] = rangedDistance >> 8;
+  buff[19] = rangedDistance;
+
+  Wire.write(buff, 20);
 }
 
 
